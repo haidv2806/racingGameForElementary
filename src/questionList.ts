2f5a6a4ef6c0666cbfead01f093af47b56655cbe
Đỗ Văn Hải <haidv28062004@gmail.com>
2025-08-21 00:18:08 +0700

    thêm câu hỏi

--- src/questionList.ts
+++ src/questionList.ts
@@ -6,36 +6,36 @@ const questionList = {
             "questions": "Có bao nhiêu hình vuông?",
             "answer": "Có 6 hình vuông."
         },
-        // {
-        //     "title": "Chặng 1",
-        //     "heading": "Quan sát biểu đồ và trả lời câu hỏi",
-        //     "questions": "Có bao nhiêu hình tròn?",
-        //     "answer": "Có 8 hình tròn."
-        // },
-        // {
-        //     "title": "Chặng 1",
-        //     "heading": "Quan sát biểu đồ và trả lời câu hỏi",
-        //     "questions": "Có bao nhiêu hình tam giác?",
-        //     "answer": "Có 4 hình tam giác."
-        // },
-        // {
-        //     "title": "Chặng 1",
-        //     "heading": "Quan sát biểu đồ và trả lời câu hỏi",
-        //     "questions": "Có bao nhiêu hình chữ nhật?",
-        //     "answer": "Có 5 hình chữ nhật."
-        // },
-        // {
-        //     "title": "Chặng 1",
-        //     "heading": "Quan sát biểu đồ và trả lời câu hỏi",
-        //     "questions": "Hình nào có nhiều nhất?",
-        //     "answer": "Hình tròn có nhiều nhất."
-        // },
-        // {
-        //     "title": "Chặng 1",
-        //     "heading": "Quan sát biểu đồ và trả lời câu hỏi",
-        //     "questions": "Hình nào có ít nhất?",
-        //     "answer": "Hình tam giác có ít nhất."
-        // },
+        {
+            "title": "Chặng 1",
+            "heading": "Quan sát biểu đồ và trả lời câu hỏi",
+            "questions": "Có bao nhiêu hình tròn?",
+            "answer": "Có 8 hình tròn."
+        },
+        {
+            "title": "Chặng 1",
+            "heading": "Quan sát biểu đồ và trả lời câu hỏi",
+            "questions": "Có bao nhiêu hình tam giác?",
+            "answer": "Có 4 hình tam giác."
+        },
+        {
+            "title": "Chặng 1",
+            "heading": "Quan sát biểu đồ và trả lời câu hỏi",
+            "questions": "Có bao nhiêu hình chữ nhật?",
+            "answer": "Có 5 hình chữ nhật."
+        },
+        {
+            "title": "Chặng 1",
+            "heading": "Quan sát biểu đồ và trả lời câu hỏi",
+            "questions": "Hình nào có nhiều nhất?",
+            "answer": "Hình tròn có nhiều nhất."
+        },
+        {
+            "title": "Chặng 1",
+            "heading": "Quan sát biểu đồ và trả lời câu hỏi",
+            "questions": "Hình nào có ít nhất?",
+            "answer": "Hình tam giác có ít nhất."
+        },
     ],
     "type_2": [
         {
@@ -44,30 +44,30 @@ const questionList = {
             "questions": "Có bao nhiêu hoa hồng?",
             "answer": "Có 6 hoa hồng."
         },
-        // {
-        //     "title": "Chặng 2",
-        //     "heading": "Quan sát biểu đồ và trả lời câu hỏi",
-        //     "questions": "Có bao nhiêu hoa cúc?",
-        //     "answer": "Có 4 hoa cúc."
-        // },
-        // {
-        //     "title": "Chặng 2",
-        //     "heading": "Quan sát biểu đồ và trả lời câu hỏi",
-        //     "questions": "Có bao nhiêu hoa đồng tiền?",
-        //     "answer": "Có 10 hoa đồng tiền."
-        // },
-        // {
-        //     "title": "Chặng 2",
-        //     "heading": "Quan sát biểu đồ và trả lời câu hỏi",
-        //     "questions": "Hoa nào có nhiều nhất?",
-        //     "answer": "Hoa đồng tiền có nhiều nhất."
-        // },
-        // {
-        //     "title": "Chặng 2",
-        //     "heading": "Quan sát biểu đồ và trả lời câu hỏi",
-        //     "questions": "Hoa nào có ít nhất?",
-        //     "answer": "Hoa cúc có ít nhất."
-        // },
+        {
+            "title": "Chặng 2",
+            "heading": "Quan sát biểu đồ và trả lời câu hỏi",
+            "questions": "Có bao nhiêu hoa cúc?",
+            "answer": "Có 4 hoa cúc."
+        },
+        {
+            "title": "Chặng 2",
+            "heading": "Quan sát biểu đồ và trả lời câu hỏi",
+            "questions": "Có bao nhiêu hoa đồng tiền?",
+            "answer": "Có 10 hoa đồng tiền."
+        },
+        {
+            "title": "Chặng 2",
+            "heading": "Quan sát biểu đồ và trả lời câu hỏi",
+            "questions": "Hoa nào có nhiều nhất?",
+            "answer": "Hoa đồng tiền có nhiều nhất."
+        },
+        {
+            "title": "Chặng 2",
+            "heading": "Quan sát biểu đồ và trả lời câu hỏi",
+            "questions": "Hoa nào có ít nhất?",
+            "answer": "Hoa cúc có ít nhất."
+        },
     ]
 };
 
